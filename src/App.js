3ec3525a1bc7f5f06a18e34a0bc6e89943cc4a01
Jean Agostinho <jeeanagos@gmail.com>
2021-03-10 10:14:27 -0300

    Novos filtros adicionados

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { useState } from 'react'
 import { ReactComponent as Robot } from '../src/images/robot.svg'
 import './App.css'
 import gifCarregando from '../src/images/loading.gif'
+import {MdAddAPhoto, MdClear} from "react-icons/md"
 
 //JSX - Java Script eXtension
 //Hook
 function App(){
   const[pessoas, setPessoas] = useState([])
   const[carregando, setCarregando] = useState(false)
+  const[idade, setIdade] = useState('')
+  const[etnia, setEtnia] = useState('')
+  const estiloIcone = {background: 'cadetblue', fontSize: '1.1em'}
 
   function ListaPessoas(){
     const listagemPessoas= pessoas.map((pessoa)=>
@@ -21,7 +25,18 @@ function App(){
 
   async function obterFoto(){
     setCarregando(true)
-    let url = "https://api.generated.photos/api/v1/faces?api_key=UJsjMuZKYBxmRAbGEvXjng"
+    let chaveAPI = process.env.REACT_APP_APIKEY
+
+    const filtraEtnia = etnia.length > 0 ? `&ethnicity=${etnia}` : ''
+    const filtraIdade = etnia.length > 0 ? `&age=${idade}` : ''
+    /* Versão extensa do comando acima
+    if(etnia.legnth > 0 ){
+      filtraEtnia = `&ethnicity =${etnia}`
+    else {
+      filtraEtnia = ''
+      }
+    */
+    let url = `https://api.generated.photos/api/v1/faces?api_key=${chaveAPI}${filtraEtnia}${filtraIdade}&order_by=random`
     await fetch(url)
     .then(response => response.json())
     .then (data => {
@@ -41,12 +56,41 @@ function App(){
       {carregando &&
         <img src= {gifCarregando} title="Aguarde..." alt="Aguarde, dados sendo carregados" />
       }
-      <div className= "fotos">
-      <ListaPessoas />
+      <div className= "linha">
+            <ListaPessoas />
+            </div>
+      
+      <div className="linha">
+          <label>Idade:</label>
+          <select onChange={event => setIdade(event.target.value)}>
+            <option value="">Todas</option>
+            <option value="infant">Infantil</option>
+            <option value="child">Criança</option>
+            <option value="young-adult">Jovem</option>
+            <option value="adult">Adulo</option>
+            <option value="elderly">Idoso</option>
+          </select>
+          <label>Etinia:</label>
+          <select onChange={e => setEtnia(e.target.value)}>
+            <option value="">Todas</option>
+            <option value="latino">Latina</option>
+            <option value="asian">Asiática</option>
+            <option value="white">Branca</option>
+            <option value="black">Negra</option>
+          </select>
       </div>
-      <button type='button' onClick={obterFoto}>
-        Obter Imagens
-      </button>
+
+            <div className="linha">
+            <button type='button' onClick={obterFoto}>
+              <MdAddAPhoto style={estiloIcone}/> Obter Imagens
+            </button>
+            {/* Exemplo de chamada de uma arrow function */}
+            {pessoas.length > 0 &&
+            <button type='button' onClick={() => setPessoas([])}>
+              <MdClear style={estiloIcone}  /> Limpar Imagens
+            </button>
+            }
+      </div>
     </div>
   )
 }
